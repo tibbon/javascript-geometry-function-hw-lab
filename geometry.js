666ee14e9a8a91b95f9dbcb0bdf5fa8c1005218f
Wesley Carr <surfswell17@gmail.com>
2013-07-22 17:16:32 -0400

    Working on obtuse equation.

--- geometry.js
+++ geometry.js
@@ -7,7 +7,42 @@ var rectangle = {
 
 // Triangle
 var triangle = {
-  sideA: 3,
-  sideB: 4,
-  sideC: 4
+  A: 3,
+  B: 4,
+  C: 4
 };
+
+// Rectangle Functions
+function isSquare(length, width) {
+  if (length === width) {
+    console.log("That there is a square");
+  } else {
+    return "That there is not square";
+  }
+}
+function squArea(length, width) {
+  return length * width;
+}
+function periMeter(length, width) {
+  return length + length + width + width;
+}
+
+// Triangle Functions
+function isEqui(A, B, C) {
+  if (A === B && A === C) {
+    console.log("EquiFine");
+  } else {
+    return "Not EquiFine";
+  }
+}
+function isIso(A, B, C) {
+  if (A === B || C) {
+    console.log("That's Nisosceles");
+  } else {
+    return "Not very Nisosceles";
+  }
+}
+function triArea(A, B, C) {
+  var area = ((A + B + C) * 0.5);
+  return Math.sqrt(area(area - A)(area - B)(area - C));
+}
