5cdbc3c2453b94fc741e800d53b6c262698f9270
Ben Israeli <jisraeli@gmail.com>
2013-07-22 17:18:51 -0400

    square functions working appropriately - questions about syntax though as I may have merely gotten lucky here

--- geometry.js
+++ geometry.js
@@ -1,7 +1,19 @@
 // Rectangle
 var rectangle = {
   length: 4,
-  width: 4
+  width: 4,
+  area: function() {
+    console.log(this.length * this.width);
+  },
+  perimeter: function() {
+    console.log(this.length + this.length + this.width + this.width);
+  },
+  isSquare: function() {
+    if (this.width == this.length)
+      return "Nice Square!"
+    else
+      return "Not a square!"
+  }
 };
 
 
