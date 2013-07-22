efc4583c00f13a3f8176a1de5527773a46f7b515
Christopher Wright <cmwright33@gmail.com>
2013-07-22 17:13:17 -0400

    took the functions out of the example triagle and square added a couple more functions

--- geometry.js
+++ geometry.js
@@ -1,23 +1,30 @@
 // Rectangle
 var rectangle = {
   length: 4,
-  width: 4,
-  isSquare: function(){
-    if
-      (this.length / this.width === 0)
-      console.log("that is a square");
-    else
-      (console.log("that is not a square"));
-  },
-  area: function(){
-    (result = this.length * this.width);
-    return result;
-  },
-  perimeter: function(){
-    result = (this.length * 2) + (this.width * 2);
-    return result;
-  }
+  width: 4
 };
+
+var msg = prompt("give me some parameters?");
+
+
+function isSquare(length, width){
+  if(this.length / this.width === 0)
+    console.log("that is a square");
+  else
+    (console.log("that is not a square"));
+}
+
+
+function area(){
+  (result = this.length * this.width);
+  return result;
+}
+
+
+function perimeter(){
+  result = (this.length * 2) + (this.width * 2);
+  return result;
+}
 
 
  console.log(rectangle.isSquare);
@@ -30,11 +37,22 @@ var triangle = {
   sideA: 3,
   sideB: 4,
   sideC: 4
-  isEquallateral: function(){
-    if (sideA === sideB|| sideA === sideC || sideB)
-
-  }
-
-
-
 };
+
+function isIsosceles(){
+  if (sideA === sideB || sideA === sideC || sideB === sideC);
+    console.log("this is a isosceles triangle");
+  else
+    console.log("this is not an isosceles");
+}
+
+
+
+function isEquallateral(){
+  if (sideC === sideB === sideA)
+      console.log("this is an equallateral");
+    else
+      console.log("this is not equallateral");
+}
+
+
