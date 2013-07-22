f4b05c64a1e7d6cccb2da4b8f9b5166d752b485e
Christopher Wright <cmwright33@gmail.com>
2013-07-22 16:53:50 -0400

    finished square functions, moving to triangle

--- geometry.js
+++ geometry.js
@@ -1,8 +1,28 @@
 // Rectangle
 var rectangle = {
   length: 4,
-  width: 4
+  width: 4,
+  isSquare: function(){
+    if
+      (this.length / this.width === 0)
+      console.log("that is a square");
+    else
+      (console.log("that is not a square"));
+  },
+  area: function(){
+    (result = this.length * this.width);
+    return result;
+  },
+  perimeter: function(){
+    result = (this.length * 2) + (this.width * 2);
+    return result;
+  }
 };
+
+
+ console.log(rectangle.isSquare);
+
+
 
 
 // Triangle
@@ -10,4 +30,11 @@ var triangle = {
   sideA: 3,
   sideB: 4,
   sideC: 4
+  isEquallateral: function(){
+    if (sideA === sideB|| sideA === sideC || sideB)
+
+  }
+
+
+
 };
