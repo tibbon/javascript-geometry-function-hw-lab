bba35452bda191c413b3ba6125f39f10bcf11398
Alina Guzman <alinochka8@gmail.com>
2013-07-22 16:50:49 -0400

    Wrote the three rectangle functions and first triangle function

--- geometry.js
+++ geometry.js
@@ -4,6 +4,24 @@ var rectangle = {
   width: 4
 };
 
+//isSquare - Returns whether the rectangle is a square or not
+function isSquare(rectangle){
+   if (rectangle.length == rectangle.width) {
+       return "The rectangle is a square"
+   } else {
+       return "The rectangle is not a square"
+   }
+}
+
+//area - Returns the area of the rectangle
+function area(rectangle){
+    return rectangle.length * rectangle.width;
+}
+
+//perimeter - Returns the perimeter of the rectangle
+function perimeter(rectangle){
+    return (2*rectangle.length) + (2*rectangle.width);
+}
 
 // Triangle
 var triangle = {
@@ -11,3 +29,31 @@ var triangle = {
   sideB: 4,
   sideC: 4
 };
+
+//isEquilateral - Returns whether the triangle is equilateral or not
+function isEquilateral(triangle){
+   if (triangle.sideA == triangle.sideB && triangle.sideB == triangle.sideC){
+      console.log("This triangle is equilateral")
+   } else {
+       console.log("This triangle isn't equilateral")
+   }
+}
+
+//isIsosceles - Returns whether the triangle is isosceles or not
+function isIsosceles(triangle){
+   if (triangle.sideA == triangle.sideB && triangle.sideB != triangle.sideC)
+       console.log("This triangle is isosceles");
+   } else if (triangle.sideB == sideC) {
+
+   }
+}
+
+//area - Returns the area of the Triangle
+function area(triangle){
+
+}
+
+//isObtuse - Returns whether the triangle is obtuse or not
+function isObtuse(triangle){
+
+}
