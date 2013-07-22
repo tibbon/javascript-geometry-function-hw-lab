123e9cd327304498a1a130229cbac4376ea75cd8
Jamal Powell <jamalkpowell@gmail.com>
2013-07-22 16:50:40 -0400

    working on rectangle

--- geometry.js
+++ geometry.js
@@ -1,8 +1,21 @@
 // Rectangle
 var rectangle = {
   length: 4,
-  width: 4
+  width: 4,
+  area: function() {
+          console.log(this.length * this.width);
+        },
+  perimeter: function() {
+    console.log((this.length * 2) + (this.width * 2));
+        },
+  isSquare: function(){
+   if {this.width == this.length};
+    return "Yup, thats a Square";
+   else
+    return "Nah..Thats something else. You weird";
+  };
 };
+
 
 
 // Triangle
