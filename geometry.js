bc5e52b06bb0abc70e7ff989ac7302b09410b554
eleyna <eleyna@gmail.com>
2013-07-22 21:27:43 -0400

    done

--- geometry.js
+++ geometry.js
@@ -3,14 +3,10 @@ var rectangle = {
   length: 4,
   width: 5,
   isSquare: function() {
-    if (this.length == this.width) {
-       return true;
-     } else {
-       return false;
-     }
+    return this.length == this.width;
    },
   area: function() {return this.length * this.width;},
-  perimeter: function() {return this.length * 2 + this.width * 2;}
+  perimeter: function() {return (this.length + this.width) * 2;}
 };
 
 // Triangle
@@ -19,29 +15,18 @@ var triangle = {
   sideB: 4,
   sideC: 4,
   isEquilateral: function() {
-    if (this.sideA == this.sideB == this.sideC) {
-      return true;
-    } else {
-      return false;
-    }
+    return this.sideA == this.sideB == this.sideC;
   },
   isIsosceles: function() {
-    if (this.sideA == this.sideB || this.sideA == this.sideC || this.sideB == this.sideC) {
-      return true;
-    } else {
-      return false;
-    }
+    return this.sideA == this.sideB || this.sideA == this.sideC || this.sideB == this.sideC;
   },
   area: function() {
     var s = (this.sideA + this.sideB + this.sideC)/2;
     return Math.sqrt(s * (s - this.sideA) * (s - this.sideB) * (s - this.sideC));},
   isObtuse: function() {
-    if (Math.pow(this.sideA, 2) + Math.pow(this.sideB, 2) < Math.pow(this.sideC, 2) ||
-        Math.pow(this.sideA, 2) + Math.pow(this.sideC, 2) < Math.pow(this.sideB, 2) ||
-        Math.pow(this.sideB, 2) + Math.pow(this.sideC, 2) < Math.pow(this.sideA, 2)) {
-      return true;
-    } else
-    return false;
+    return Math.pow(this.sideA, 2) + Math.pow(this.sideB, 2) < Math.pow(this.sideC, 2) ||
+           Math.pow(this.sideA, 2) + Math.pow(this.sideC, 2) < Math.pow(this.sideB, 2) ||
+           Math.pow(this.sideB, 2) + Math.pow(this.sideC, 2) < Math.pow(this.sideA, 2);
   }
 };
 
